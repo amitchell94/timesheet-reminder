0a2858ebd7b3e93a973c8fea84d075e7ad9c8041
andym <andym@DESKTOP-F3F9PR6>
2019-04-06 19:40:11 +0100

    Add project files.

---
+++ ReminderApp/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ReminderApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.timesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Time";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(22, 323);
+            this.maskedTextBox1.Mask = "00:00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(177, 62);
+            this.maskedTextBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AM",
+            "PM"});
+            this.comboBox1.Location = new System.Drawing.Point(205, 321);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(65, 63);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 457);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(484, 571);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 35);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "V";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Timesheet Remind now minimised.";
+            this.notifyIcon1.Text = "Timesheet Reminder";
+            this.notifyIcon1.Visible = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(296, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 64);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Add Time";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timesListBox
+            // 
+            this.timesListBox.FormattingEnabled = true;
+            this.timesListBox.ItemHeight = 25;
+            this.timesListBox.Location = new System.Drawing.Point(22, 88);
+            this.timesListBox.Name = "timesListBox";
+            this.timesListBox.Size = new System.Drawing.Size(278, 154);
+            this.timesListBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 618);
+            this.Controls.Add(this.timesListBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time Sheet Reminder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox timesListBox;
+    }
+}
+
---
+++ ReminderApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace ReminderApp
+{
+
+    
+    public partial class Form1 : Form
+    {
+        string currentTime;
+        string messageTime;
+        //List<ReminderTime> reminderTimes = new List<ReminderTime>();
+        private Dictionary<string, DateTime> reminderTimes = new Dictionary<string, DateTime>();
+        bool complete = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer2.Start();
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+
+            currentTime = DateTime.Now.ToString("hh:mm:ss tt");
+            label1.Text = currentTime;
+            TimeSpan fiveMinutes = new TimeSpan(0, 5 ,0);
+            DateTime fiveMinutesAgo = DateTime.Now.Subtract(fiveMinutes);
+
+            if (reminderTimes.ContainsKey(currentTime) && ((DateTime.Compare(reminderTimes[currentTime], fiveMinutesAgo)) < 0))
+            {
+                reminderTimes[currentTime] = DateTime.Now;
+                string res = Interaction.InputBox("What are you doing right now?");
+
+                try
+                {
+
+                    if (!complete)
+                    {
+                        StreamWriter sw = new StreamWriter("C:\\Users\\andym\\Documents\\Sample.txt");
+
+                        sw.WriteLine("Timesheet entries");
+                        sw.Close();
+                        complete = true;
+                    }
+
+                    File.AppendAllText(@"C:\\Users\\andym\\Documents\\Sample.txt",
+                        DateTime.Now.DayOfWeek.ToString() + "\t" +
+                        DateTime.Now.ToString() + "\t" + res + Environment.NewLine);
+
+                }
+                catch (Exception exception)
+                {
+                    System.Console.Write(exception.StackTrace);
+                }
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            messageTime = maskedTextBox1.Text + " " + comboBox1.Text;
+            reminderTimes.Add(messageTime, DateTime.MinValue);
+            timesListBox.Items.Add(messageTime);
+        }
+    }
+}
